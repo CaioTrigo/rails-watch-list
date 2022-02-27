bd89ee77a77d6225737639961a2d38109da72c81
caio trigo <caiodstrigo@gmail.com>
2022-02-27 18:40:29 -0300

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i[index create show new] do
+    resources :bookmarks, only: %i[create new]
+  end
+
+  resources :bookmarks, only: :destroy
+  # resources :lists, only: :destroy
+
+  root to: 'lists#index'
 end
