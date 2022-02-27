0e5b7530da1a2784277e226c42075c1d9a24b523
caio trigo <caiodstrigo@gmail.com>
2022-02-27 18:35:49 -0300

    Movies API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+URI.open("http://tmdb.lewagon.com/movie/top_rated") do |lines|
+  lines.each_line do |line|
+    results = JSON.parse(line)
+    results["results"].each do |result|
+      if result["original_language"] == 'en'
+        # title, vote_average, overview, poster_url
+        image = "https://image.tmdb.org/t/p/original#{result["poster_path"]}"
+        Movie.create(
+          title: result["title"],
+          rating: result["vote_average"],
+          overview: result["overview"],
+          poster_url: image
+        )
+      end
+    end
+  end
+end
